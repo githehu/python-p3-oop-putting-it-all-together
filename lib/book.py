16bed969aff79002cb0d1e8bf64f8e2c5418258f
githehu <githehudavid888@gmail.com>
2025-10-17 13:17:01 +0300

    firstcommit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,35 @@
-#!/usr/bin/env python3
+# lib/book.py
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    # title getter and setter
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise Exception("Title must be a string")
+        self._title = value
+
+    # page_count getter and setter
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            raise Exception("Page count must be an integer")
+        self._page_count = value
+
+    # method for reading
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
-#!/usr/bin/env python3
+# lib/shoe.py
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"
+
+    # brand getter and setter
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if not isinstance(value, str):
+            raise Exception("Brand must be a string")
+        self._brand = value
+
+    # size getter and setter
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (int, float)):
+            raise Exception("Size must be a number")
+        self._size = value
+
+    def cobble(self):
+        """Repairs the shoe and sets condition to 'New'."""
+        self.condition = "New"
+        print("Your shoe is as good as new!")
